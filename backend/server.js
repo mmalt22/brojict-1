1304821be337a5eda5e55ce1b3693dff746c7853
mmalt22 <hossam.almatrfy.77@gmail.com>
2022-05-30 05:52:58 +0300

    finish User, Login, Register

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
 
 const db = require("./db");
 const Todo = require("./Todo");
+const User = require("./user");
 // console.log(Todo)
 
 // touch .gitignore
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json("GET / is Working ...");
@@ -82,8 +83,8 @@ app.delete("/tasks/:id", (req, res) => {
       console.log("ERROR: ", err);
     } else {
       deleteObj.deletedCount === 1
-        ? res.json("Delete this todo successfully")
-        : res.status(404).json("This todo is not found");
+        ? res.json({message: "Delete this todo successfully"})
+        : res.status(404).json({message: "This todo is not found"});
     }
   });
 });
@@ -96,8 +97,8 @@ app.delete("/tasks", (req, res) => {
     } else {
       console.log(deleteObj);
       deleteObj.deletedCount === 0
-        ? res.status(404).json("There is no completed todo found")
-        : res.json("Delete all completed todo successfully");
+        ? res.status(404).json({message: "There is no completed todo found"})
+        : res.json({message: "Delete all completed todo successfully"});
     }
   });
 });
@@ -113,8 +114,8 @@ app.put("/tasks/:id", (req, res) => {
       } else {
         console.log(updateObj);
         updateObj.modifiedCount === 1
-          ? res.json("update this todo successfully")
-          : res.status(404).json("This todo is not found");
+          ? res.json({message: "update this todo successfully"})
+          : res.status(404).json({message: "This todo is not found"});
       }
     }
   );
@@ -123,18 +124,59 @@ app.put("/tasks/:id", (req, res) => {
 app.put("/tasks/:id/:isCompleted", (req, res) => {
   console.log("125", req.params);
   Todo.updateOne(
-    { _id: req.params.id },{ isCompleted: req.params.isCompleted },(err, updateObj) => {
+    { _id: req.params.id },
+    { isCompleted: req.params.isCompleted },
+    (err, updateObj) => {
       if (err) {
         // console.log("ERROR: ", err);
         res.status(400).json(err);
       } else {
         // console.log(updateObj);
         updateObj.modifiedCount === 1
-          ? res.json("update this todo successfully")
-          : res.status(404).json("This todo is not found");
+          ? res.json({message: "update this todo successfully"})
+          : res.status(404).json({message: "This todo is not found"});
       }
     }
   );
+});
+
+app.post("/users/Register", (req, res) => {
+  console.log("28:", req.body);
+  User.create(req.body, (err, newUser) => {
+    if (err) {
+      // console.log("ERROR: ", err);
+      res.status(400).json({ message: "This email already taken" });
+    } else {
+      // res.status(201).json(newUser);
+      res.status(201).json({message: "Create New User successfully",});
+    }
+  });
+});
+
+app.post("/users/login", (req, res) => {
+  User.find({ email: req.body.email }, (err, arrUserFound) => {
+    if (err) {
+      console.log("ERROR: ", err);
+    } else {
+      // console.log(arrUserFound)
+      if (arrUserFound.length === 1) {
+        if (req.body.password === arrUserFound[0].password) {
+          // password correct
+          res.status(200).json({
+            message: "login soccessfully",
+            username: arrUserFound[0].username,
+          });
+        } else {
+          // password incorrect
+          res.status(400).json({ message: "Wrong password" });
+        }
+      } else {
+        res.status(404).json({
+          message: "The email entered is not register",
+        });
+      }
+    }
+  });
 });
 
 app.listen(5000, () => {
